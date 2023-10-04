dc1df5cda140d932e119992f365dee78c719087d
MayaGaS <11mayagarrido.11@gmail.com>
2023-10-04 16:28:50 -0600

    Parte 1 exam1P vistas, plantillas

---
+++ exam1P/resources/views/layouts/partials/vista2.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.plantilla')
+
+@section('titulo', 'Vista 2')
+
+@section('contenido')
+
+<h1 class="display-1 text-center text-danger mt-5"> Vista 2 </h1>
+
+@endsection
---
+++ exam1P/resources/views/layouts/partials/vista3.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.plantilla')
+
+@section('titulo', 'Vista 3')
+
+@section('contenido')
+
+<h1 class="display-1 text-center text-danger mt-5"> Vista 3 </h1>
+
+@endsection
